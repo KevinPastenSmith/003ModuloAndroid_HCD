513bffd1c6fddfa05370f73e0d7cc99a666dc387
KevinPastenSmith <kevin.pastensmith@gmail.com>
2021-07-21 22:29:27 -0400

    cambio final del dia

--- app/src/main/java/com/example/helical_conveyor_design/Invitado.java
+++ app/src/main/java/com/example/helical_conveyor_design/Invitado.java
@@ -26,7 +26,6 @@ public class Invitado extends AppCompatActivity {
         btn1_invitado_nuevo = (Button) findViewById(R.id.btn_invitado_nuevo);
         btn2_invitado_adminProyectActual = (Button) findViewById(R.id.btn_invitado_adminproyectactual);
 
-        holaInvitado(et1_invitado_nickName);
     }
 
     //Metodo button "nuevo"
--- app/src/main/java/com/example/helical_conveyor_design/Nuevo.java
+++ app/src/main/java/com/example/helical_conveyor_design/Nuevo.java
@@ -3,24 +3,25 @@ package com.example.helical_conveyor_design;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Nuevo extends AppCompatActivity {
 
-    private TextView tvn1, tvn2, tvn3, tvn4, tvn5, tvn6, tvn7;
+    private Button btn1_nuevoProyecto, btn2_nuevoSisTransHeli, btn3_nuevoMaterial, btn4_nuevoMotor, btn5_nuevoTransportador, btn6_nuevaHelice, btn7_nuevoTubo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo);
 
-        tvn1 = (TextView) findViewById(R.id.tv_nuevo_nuevoProyecto);
-        tvn2 = (TextView) findViewById(R.id.tv_nuevo_nuevoSisTransHeli);
-        tvn3 = (TextView) findViewById(R.id.tv_nuevo_nuevoMaterial);
-        tvn4 = (TextView) findViewById(R.id.tv_nuevo_nuevoMotor);
-        tvn5 = (TextView) findViewById(R.id.tv_nuevo_nuevoTransportador);
-        tvn6 = (TextView) findViewById(R.id.tv_nuevo_nuevaHelice);
-        tvn7 = (TextView) findViewById(R.id.tv_nuevo_nuevoTubo);
+        btn1_nuevoProyecto = (Button) findViewById(R.id.btn_nuevo_nuevoproyecto);
+        btn2_nuevoSisTransHeli = (Button) findViewById(R.id.btn_nuevo_nuevoSistTransHeli);
+        btn3_nuevoMaterial = (Button) findViewById(R.id.btn_nuevo_nuevomaterial);
+        btn4_nuevoMotor = (Button) findViewById(R.id.btn_nuevo_nuevomotor);
+        btn5_nuevoTransportador = (Button) findViewById(R.id.btn_nuevo_nuevotransportador);
+        btn6_nuevaHelice = (Button) findViewById(R.id.btn_nuevo_nuevahelice);
+        btn7_nuevoTubo = (Button) findViewById(R.id.btn_nuevo_nuevotubo);
 
     }
 }
